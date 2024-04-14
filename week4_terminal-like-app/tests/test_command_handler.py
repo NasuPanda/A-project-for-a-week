bd72199b8070b684f0c8566f769274ef6f25d77b
NasuPanda <xiyuanjunzhi@gmail.com>
2024-04-14 11:21:29 +0900

    test: Add a test to check if valid commands do not raise error

--- week4_terminal-like-app/tests/test_command_handler.py
+++ week4_terminal-like-app/tests/test_command_handler.py
@@ -1,9 +1,17 @@
 import pytest
+
 from command_handler import handle_command
+from constants import ALLOWED_COMMANDS
+
 
 def test_exit_handler():
     result = handle_command("exit")
     assert result == False
+
+@pytest.mark.parametrize("command", ALLOWED_COMMANDS)
+def test_valid_command_do_not_raise_error(command):
+    # This will be enough to test that no error is raised
+    result = handle_command(command)
 
 def test_invalid_command():
     with pytest.raises(ValueError) as exception_info:
