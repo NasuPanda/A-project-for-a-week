beec3b9dd4fcf972fe8a514bfe373fecbe5ed5f7
NasuPanda <xiyuanjunzhi@gmail.com>
2024-04-14 13:43:36 +0900

    test: Add tests for ls commands

--- week4_terminal-like-app/tests/test_ls.py
+++ week4_terminal-like-app/tests/test_ls.py
@@ -1,5 +1,14 @@
-import ls
+import pytest
+from ls import ls
 
-def test_ls():
-    # This will test our ls.py.
-    ls.ls()
+# TODO Implement this
+cwd = "./"
+
+@pytest.fixture
+def empty_filesystem():
+    # Here you would set up your mock file system, however you decide to represent it
+    return {}  # An empty dictionary, for a start
+
+def test_ls_when_cwd_is_empty(empty_filesystem):
+    result = ls(cwd)
+    assert result == ""
