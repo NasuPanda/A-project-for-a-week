ec9878ad1451e3b42cb4625c36e02dc0f8af3282
NasuPanda <xiyuanjunzhi@gmail.com>
2024-04-14 19:04:45 +0900

    test: modify test to test CommandHandler class

--- week4_terminal-like-app/tests/test_command_handler.py
+++ week4_terminal-like-app/tests/test_command_handler.py
@@ -1,21 +1,23 @@
 import pytest
 
-from command_handler import handle_command, ALLOWED_COMMANDS
+from command_handler import CommandHandler
 
+@pytest.fixture
+def command_handler() -> CommandHandler:
+    return CommandHandler()
 
-def test_exit_handler():
-    result = handle_command("exit")
+def test_exit_handler(command_handler: CommandHandler):
+    result = command_handler.handle_command("exit")
     assert result == False
 
-@pytest.mark.parametrize("command", ALLOWED_COMMANDS)
-def test_valid_command_do_not_raise_error(command):
-    # This will be enough to test that no error is raised
-    result = handle_command(command.name)
-
-def test_invalid_command():
+def test_invalid_command(command_handler: CommandHandler):
     with pytest.raises(ValueError) as exception_info:
-        handle_command("some_unknown_command")
+        command_handler.handle_command("some_unknown_command")
 
     # Get an error message form exception_info
     error_message = str(exception_info.value)
     assert error_message == "Command not found"
+
+def test_valid_command_do_not_raise_error(command_handler: CommandHandler):
+    # This will be enough to test that no error is raised
+    [command_handler.handle_command(command.name) for command in command_handler.allowed_commands]
